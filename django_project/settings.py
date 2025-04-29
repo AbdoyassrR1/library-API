a611cd777ed52710e19b00ab5c74381477a744ed
Abdo YasserR <abdoyaser2018a@gmail.com>
2025-04-29 07:25:05 +0300

    New Updates for render.com Deployment

--- django_project/settings.py
+++ django_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-68quk=r$a0yl&p=rzcokygbtnlq)%k3$#gbr8t#ns#tn4y!+!^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [".herokuapp.com", "localhost", "127.0.0.1"]
+ALLOWED_HOSTS = [".onrender.com", "localhost", "127.0.0.1"]
 
 
 # Application definition
